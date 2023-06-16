313dbb35393e9e8a1882ff0acc5f093d3cbd921f
Ramiz Zarzor <ramizzarzor1998@gmail.com>
2023-06-16 17:15:57 +0300

    FOR_DEPLOY

--- frontend/src/Redux/productsApi.jsx
+++ frontend/src/Redux/productsApi.jsx
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
   reducerPath: 'productsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://onlineshop911.onrender.com/' }),
   endpoints: (builder) => ({
     getproductsByName: builder.query({
       query: (name) => `products`,
@@ -16,7 +16,7 @@ export const productsApi = createApi({
 // Get only 1 Product
 export const oneproductsApi = createApi({
   reducerPath: 'oneproductsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://onlineshop911.onrender.com/' }),
   endpoints: (builder) => ({
     getOneProduct: builder.query({
       query: (name) => `products/${name}`,
